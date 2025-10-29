6223d119900802bc9cae78ab0494fd93c0b66d2b
RajatRawat10 <rajatrawat4242@gmail.com>
2025-10-29 15:17:40 +0530

    Completed Gallery Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,53 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import Imgcards from './components/Imgcards'
 
 const App = () => {
 
   const [userData, setUserData] = useState([])
+  const [index, setIndex] = useState(1)
 
   const getdata = async () => {
-    const response = await axios.get("https://picsum.photos/v2/list?page=2&limit=30")
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=18`)
     setUserData(response.data)
-    // console.log(userData)
   }
 
-  let printuserdata = "No User Avaiable"
+  useEffect(() => {
+    getdata();
+  }, [index])
+
+
+  let printuserdata = <h3 className='text-gray-400 absolute top-1/2 left-1/2 text-2xl -translate-x-1/2 -translate-y-1/2'>loading...</h3>
   if (userData.length > 0) {
     printuserdata = userData.map(function (elem, idx) {
       return <div key={idx}>
-        <img className='h-40' src={elem.download_url} alt="" />
+        <Imgcards elem={elem} />
       </div>
     })
   }
 
-
   return (
     <div className='bg-black h-screen overflow-auto p-5 text-white'>
-      <button onClick={getdata} className='bg-green-600 px-6 active:scale-95 py-2 rounded text-white'>Get Data</button>
-      <div className='flex flex-wrap gap-4'>{printuserdata}</div>
+      <div className='flex flex-wrap justify-center gap-3 '>{printuserdata}</div>
+      <div className='flex gap-4 items-center justify-center mt-4' >
+        <button style={{ opacity: index == 1 ? 0.3 : 1 }} className='bg-blue-400 text-xl text-black px-4  py-0.5 rounded cursor-pointer active:scale-95' onClick={() => {
+          if (index > 1) {
+            setIndex(index - 1)
+            setUserData([])
+          }
+        }}>
+          Prev
+        </button>
+        <h5>Page no {index}</h5>
+        <button className='bg-blue-400 text-xl text-black px-4 py-0.5 rounded cursor-pointer active:scale-95' onClick={() => {
+          setIndex(index + 1)
+          setUserData([])
 
+        }
+        }>
+          Next
+        </button>
+      </div>
     </div>
   )
 }
